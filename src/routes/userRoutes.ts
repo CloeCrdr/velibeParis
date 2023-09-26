21aea49f4f80431d674c6bba564b4e86d1ec88c4
Cloé Cordeiro <73696923+CloeCrdr@users.noreply.github.com>
2023-09-26 08:06:51 +0200

    Add authentification routes
    Co-authored-by: CentaureChris <CentaureChris@users.noreply.github.com>
    Co-authored-by: GaspardBrisset <GaspardBrisset@users.noreply.github.com>
    Co-authored-by: RandaSan <RandaSan@users.noreply.github.com>

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,5 +4,8 @@ import { login } from '../controllers/authController';
 const router = express.Router();
 
 router.post('/login', login);
+router.post('/register');
+router.post('/logout');
+router.post('/account')
 
 export default router;
